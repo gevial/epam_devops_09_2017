76b6e45fb14aec6450b8852878bca20d98a2879e
Gennadii Aleksandrov <gennadii_aleksandrov@epam.com>
2017-10-06 12:26:35 +0300

    First task solved - improvement

--- src/com/epam/Aleksandrov/FirstExercise.java
+++ src/com/epam/Aleksandrov/FirstExercise.java
@@ -6,13 +6,13 @@ public class FirstExercise {
         // args count
         // avg len of arg
         float avgLen;
-        float totalLen = 0;
-        for (int i = 0; i < args.length; i++) {
-            totalLen += args[i].length();
-        }
         if (args.length == 0) {
             avgLen = 0;
         } else {
+            float totalLen = 0;
+            for (int i = 0; i < args.length; i++) {
+                totalLen += args[i].length();
+            }
             avgLen = totalLen / args.length;
         }
         System.out.println(args.length);
