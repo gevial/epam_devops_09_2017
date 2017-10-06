e8f058cc6492e77962bb92c431298e10d20eae52
Gennadii Aleksandrov <gennadii_aleksandrov@epam.com>
2017-10-06 12:14:49 +0300

    Add first task

---
+++ src/com/epam/Aleksandrov/FirstExercise.java
@@ -0,0 +1,9 @@
+package com.epam.Aleksandrov;
+
+public class FirstExercise {
+    public static void main(String[] args) {
+        // TODO:
+        // args count
+        // avg len of arg
+    }
+}
